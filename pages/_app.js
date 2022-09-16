6378174cff8474f06805f9d1b0904069f1e33a55
Bryan Matheus <97106136+BbryanMatheusS@users.noreply.github.com>
2022-09-16 13:01:37 -0300

    html e css quase pronto . sem erros nem bugs

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,16 @@
+import '@nextcss/reset';
 import GlobalStyle from "../src/theme/GlobaStyle"
+import Header from "../src/patterns/Header/Header";
+import Footer from "../src/patterns/Footer";
+<Footer/>
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <GlobalStyle/>
+      <Header/>
       <Component {...pageProps} />
+      <Footer/>
     </>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
-import '@nextcss/reset';
 
-import Footer from "../src/patterns/Footer";
-import Header from "../src/patterns/Header/Header";
+
+
 import Hero_banner from '../src/patterns/Hero-banner';
 import Logo_bar from "../src/patterns/Logo-bar";
 import Contact from "../src/patterns/Contact-info";
@@ -17,14 +16,14 @@ export default function HomePagem(){
     return (
 
         <>
-            <Header/>
+            
             <Hero_banner/>
             <Logo_bar/>
             <Our_soluctions/>
             <Jussi_session/>
             <Contact/>
             <Newletter/>
-            <Footer/>
+            
         </>
         
     )
